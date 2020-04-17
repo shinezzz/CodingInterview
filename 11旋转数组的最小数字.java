b974f5e8bccfe38f891aff02c9b7288997624d81
shinezzz <37281856+shinezzz@users.noreply.github.com>
2020-04-17 11:12:21 +0800

    Update 11旋转数组的最小数字.java

--- 11旋转数组的最小数字.java
+++ 11旋转数组的最小数字.java
@@ -1,1 +1,24 @@
-
+// 有重复数字
+import java.util.ArrayList;
+public class Solution {
+    public int minNumberInRotateArray(int [] array) {
+        int length = array.length;
+        int low = 0, high = length-1, temp =0;
+        while(low < high){
+            temp =low + (high - low)/2;//防止溢出
+            if(array[temp] > array[high]){
+                low = temp + 1;
+            }
+            else if(array[temp] < array[high]){
+                high = temp;
+            }
+            else{
+                high = high - 1;
+            }
+            
+        }
+        return array[high];
+        
+        
+    }
+}
